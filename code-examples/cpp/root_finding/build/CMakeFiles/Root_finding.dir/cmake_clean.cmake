3104c31ca18448257a97d0039da4f39087e0c3de
Berke Mert <berke.mert@tum.de>
2024-03-05 22:59:18 +0100

    Added code for root finding (bisection method).

---
+++ code-examples/cpp/root_finding/build/CMakeFiles/Root_finding.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Root_finding.dir/root_finding.cpp.o"
+  "CMakeFiles/Root_finding.dir/root_finding.cpp.o.d"
+  "Root_finding"
+  "Root_finding.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Root_finding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
