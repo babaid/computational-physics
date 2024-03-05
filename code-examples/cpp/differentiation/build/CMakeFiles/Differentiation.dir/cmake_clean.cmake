ae67c4ca3107ded55800e532dd85fe7b5f12f162
Berke Mert <berke.mert@tum.de>
2024-03-05 18:13:03 +0100

    Added code for numerical differentiation.

---
+++ code-examples/cpp/differentiation/build/CMakeFiles/Differentiation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Differentiation.dir/differentiation.cpp.o"
+  "CMakeFiles/Differentiation.dir/differentiation.cpp.o.d"
+  "Differentiation"
+  "Differentiation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Differentiation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
